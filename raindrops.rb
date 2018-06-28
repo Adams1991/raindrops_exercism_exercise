668913d02dde4894b81733d6adfbf9cc587192cb
Adams1991 <adams121@hotmail.co.uk>
2018-06-28 12:13:06 +0100

    found better method using hash map and iterator. all tests passing

--- raindrops.rb
+++ raindrops.rb
@@ -1,4 +1,22 @@
+class Raindrops
 
+  def self.convert(number)
+    pairs = {
+       3 => "Pling",
+       5 => "Plang",
+       7 => "Plong"
+     }
+
+    message = ""
+
+    pairs.each do |(num, word)|
+      message << word if number % num == 0
+    end
+
+    message.empty? ? number.to_s : message
+  end
+
+end
 
 
 module BookKeeping
